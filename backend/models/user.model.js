691c4ec3160e5da107581fef40bb913f7dd42c59
OlgaNas <olga.vlasova321@gmail.com>
2024-03-24 12:26:37 +0000

    Added timestamps

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ""
     }
-});
+}, {timestamps: true});//createdAt and updatedAt
 
 const User = mongoose.model("User", userSchema); //Mongoose will look for or create (if it doesn't already exist) a collection named users in the database
 export default User;
