ec29bdd9f5cfc657dc6755b16fecc7dec22a9965
OlgaNas <olga.vlasova321@gmail.com>
2024-03-24 12:28:03 +0000

    Add message routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";//Express Server - better performance in applications that handle a lot of concurrent connections and I/O operations, such as real-time applications
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
@@ -11,6 +13,7 @@ dotenv.config(); //to load environment variables from a .env file into process.e
 app.use(express.json()); // to parse the incoming requests with JSON payload (from req.body)
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 // app.get("/", (req, res) => {
