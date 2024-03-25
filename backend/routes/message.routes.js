7b7f9b7cb1cc544c9595b00146c9702aaa5b34a5
OlgaNas <olga.vlasova321@gmail.com>
2024-03-25 13:14:20 +0000

    Add getMessage route

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { sendMessage, getMessages } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);//protectRoute is a middleware, to execute sendMessage function, the protectRoute function should run first
 
 export default router;
