7297386b7eb949f3b20c1067c9a8b5131cd30337
OlgaNas <olga.vlasova321@gmail.com>
2024-03-25 11:03:02 +0000

    Add router

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -3,6 +3,6 @@ import { sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.post("/send/:id", protectRoute, sendMessage);//protectRoute is a middleware
+router.post("/send/:id", protectRoute, sendMessage);//protectRoute is a middleware, to execute sendMessage function, the protectRoute function should run first
 
 export default router;
