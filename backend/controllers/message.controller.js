e9998112e90d4cd7d6c3ba82ba1a9a95246c66f4
OlgaNas <olga.vlasova321@gmail.com>
2024-03-25 13:26:33 +0000

    Update getMessage controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -55,7 +55,10 @@ export const getMessages = async (req, res) => {
             participants: { $all: [senderId, userToChatId]}
         }).populate("messages"); //The populate("messages") method in Mongoose is used to automatically replace the specified paths in the document with document(s) from other collection(s)
 
-        res.status(200).json(conversation.messages)
+        if(!conversation) return res.status(200).json([]);
+        const messages = conversation.messages;
+
+        res.status(200).json(messages);
     } catch (error) {
         console.log("Error in getMessages controller: ", error.message);
         res.status(500).json({error: "Internal server error"}); 
