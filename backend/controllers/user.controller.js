e08806529aa5be1d7cc5b48409f1a2f3ba57a559
OlgaNas <olga.vlasova321@gmail.com>
2024-03-26 10:17:38 +0000

    Add comment

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 
+//Get Users
 export const getUsersForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user._id; // we can get this thanks to protectRoute middleware
