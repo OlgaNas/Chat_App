1772dcd5c06653369dcec20e4d17715a8e0685e7
OlgaNas <olga.vlasova321@gmail.com>
2024-04-01 17:58:45 +0100

    Add Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Login from './pages/login/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='p-4 h-screen flex items-center justify-center'>
+    <Login />
+  </div> //p-4: Applies padding of 1rem (16px) on all sides of the div. h-screen: Sets the height of the div to be 100vh, making it fill the height of the screen.
+  );
 }
 
 export default App
