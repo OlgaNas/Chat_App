803d212f2afa321c1d50d962285e86de0352943c
OlgaNas <olga.vlasova321@gmail.com>
2024-04-02 18:14:19 +0100

    Create GenderCheckBox component for a SignUp page

---
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const GenderCheckbox = () => {
+  return (
+    <div className='flex'>
+        <div className="form-control">
+            <label className="label cursor-pointer gap-1">
+                <span className="label-text text-base-200">Male</span> 
+                <input type="checkbox" className="checkbox border-green-100" />
+            </label>
+        </div>
+        <div className="form-control ms-2">
+            <label className="label cursor-pointer gap-1">
+                <span className="label-text text-base-200">Female</span> 
+                <input type="checkbox" className="checkbox border-green-100" />
+            </label>
+        </div>
+    </div>
+  )
+}
+
+export default GenderCheckbox
