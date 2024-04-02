fd2793cc1eba48962d3138796b5ab36c92a3acf2
OlgaNas <olga.vlasova321@gmail.com>
2024-04-02 11:12:35 +0100

    Complete return for a Login page

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -10,9 +10,27 @@ const Login = () => {
             <form>
                 <div>
                     <label className='label p-2'>
-                        <span className='text-base label-text'>Username:</span>
-                    </label>
-                    <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10'/>
+                        <span className='text-base-200 label-text'>Username</span>
+                    </label>    
+                    <input 
+                        type='text' 
+                        placeholder='Enter username' 
+                        className='w-full input input-bordered h-10'/>
+                </div>
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base-200 label-text'>Password</span>
+                    </label>  
+                    <input 
+                        type='password' 
+                        placeholder='Enter password' 
+                        className='w-full input input-bordered h-10'/>
+                </div>
+                <a href='#' className='p-1 text-sm text-base-200 hover:underline hover:text-blue-200 mt-2 inline-block'>
+                    {"Don't"} have an account?
+                </a>
+                <div>
+                    <button className='btn btn-block btn-sm mt-3 hover:bg-green-500'>Login</button>
                 </div>
             </form>
         </div>
@@ -22,3 +40,47 @@ const Login = () => {
 
 export default Login
 
+
+
+//STARTER CODE FOR THIS FILE
+// const Login = () => {
+//     return (
+//       <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+//           <div className='w-full p-6 rounded-lg shadow-md bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+//               <h1 className='text-3xl font-semibold text-center text-gray-300'>Login 
+//               <span className='text-green-500'> ChatApp</span></h1>
+  
+//               <form>
+//                   <div>
+//                       <label className='label p-2'>
+//                           <span className='text-base-200 label-text'>Username</span>
+//                       </label>    
+//                       <input 
+//                           type='text' 
+//                           placeholder='Enter username' 
+//                           className='w-full input input-bordered h-10'/>
+//                   </div>
+//                   <div>
+//                       <label className='label p-2'>
+//                           <span className='text-base-200 label-text'>Password</span>
+//                       </label>  
+//                       <input 
+//                           type='password' 
+//                           placeholder='Enter password' 
+//                           className='w-full input input-bordered h-10'/>
+//                   </div>
+//                   <a href='#' className='p-1 text-sm text-base-200 hover:underline hover:text-blue-200 mt-2 inline-block'>
+//                       {"Don't"} have an account?
+//                   </a>
+//                   <div>
+//                       <button className='btn btn-block btn-sm mt-3 hover:bg-green-500'>Login</button>
+//                   </div>
+//               </form>
+//           </div>
+//       </div>
+//     )
+//   }
+  
+//   export default Login
+  
+  
