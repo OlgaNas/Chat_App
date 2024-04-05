8402af32ab92261731bd71b432380d809c418b77
OlgaNas <olga.vlasova321@gmail.com>
2024-04-05 14:58:49 +0100

    Add Starter Code commented to see changes later

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -20,3 +20,25 @@ const GenderCheckbox = () => {
 }
 
 export default GenderCheckbox
+
+//STARTER CODE FOR THIS FILE
+// const GenderCheckbox = () => {
+//     return (
+//       <div className='flex'>
+//           <div className="form-control">
+//               <label className="label cursor-pointer gap-1">
+//                   <span className="label-text text-base-200">Male</span> 
+//                   <input type="checkbox" className="checkbox border-green-100" />
+//               </label>
+//           </div>
+//           <div className="form-control ms-2">
+//               <label className="label cursor-pointer gap-1">
+//                   <span className="label-text text-base-200">Female</span> 
+//                   <input type="checkbox" className="checkbox border-green-100" />
+//               </label>
+//           </div>
+//       </div>
+//     )
+//   }
+  
+//   export default GenderCheckbox
