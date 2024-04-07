a43c397ab2f4b4d2e435ee4ab60def2c2cb21764
OlgaNas <olga.vlasova321@gmail.com>
2024-04-07 16:11:33 +0100

    Create Message component

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+        <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+            <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+        </div>
+        <div className="chat-bubble bg-green-900">Hello!</div>
+        <div className="chat-footer opacity-50 text-xs text-white flex gap-1 items-center">
+            12:46
+        </div>
+    </div>
+  )
+}
+
+export default Message
+
+//Start Code
+// const Message = () => {
+//   return (
+//     <div className="chat chat-end">
+//         <div className="chat-image avatar">
+//             <div className="w-10 rounded-full">
+//             <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+//             </div>
+//         </div>
+//         <div className="chat-bubble bg-green-900">Hello!</div>
+//         <div className="chat-footer opacity-50 text-xs text-white flex gap-1 items-center">
+//             12:46
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Message
