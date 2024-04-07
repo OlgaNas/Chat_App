e196fe4c6c2eb413e7b58af9e58e800626af1890
OlgaNas <olga.vlasova321@gmail.com>
2024-04-07 21:36:54 +0100

    Changed background color to gray-300

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -5,8 +5,8 @@ import { FiSearch } from "react-icons/fi";
 const SearchInput = () => {
   return (
     <form className='flex items-center gap-2 m-3 mb-5'>
-        <input type="text" placeholder='Search...' className='input input-bordered rounded-full' />
-        <button type='submit' className='btn btn-circle bg-success text-white'>
+        <input type="text" placeholder='Search...' className='input input-bordered rounded-full h-10 bg-gray-300' />
+        <button type='submit' className='btn btn-circle bg-success text-white hover:text-gray-900'>
             <FiSearch />
         </button>
     </form>
