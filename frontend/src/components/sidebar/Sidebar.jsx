70480e3522898b8009a4e0b46ea71477d70ec292
OlgaNas <olga.vlasova321@gmail.com>
2024-04-07 13:38:03 +0100

    Remove comments and add p-4

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,10 +5,8 @@ import LogoutButton from './LogoutButton'
 
 const Sidebar = () => {
   return (
-    <div className='border-r border-slate-500 flex flex-col'>
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
         <SearchInput />
-        {/* Divider will be used to separate content vertically or horizontally. */}
-        {/* <div className='divider px-3'></div> */}
         <Conversations />
         <LogoutButton />
     </div>
