67db86cbe4ccee384740537618219be98403f8f4
OlgaNas <olga.vlasova321@gmail.com>
2024-04-07 13:33:34 +0100

    Add hover for logout button

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BiLogOut } from "react-icons/bi";
 
 const LogoutButton = () => {
-  return <div className='mt-auto'><BiLogOut className='m-2 text-white cursor-pointer w-6 h-6' alt="logout" title="LogOut" /></div>
+  return <div className='mt-auto'><BiLogOut className='m-2 text-white cursor-pointer w-6 h-6 hover:text-gray-200 active:text-gray-400' alt="logout" title="LogOut" /></div>
   
 }
 
