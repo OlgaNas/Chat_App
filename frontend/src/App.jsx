6bdc3c520ceec0a0eb26d30654b69dd24c2e19ad
OlgaNas <olga.vlasova321@gmail.com>
2024-04-15 12:50:53 +0100

    Add Toaster and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
+import Home from './pages/home/Home';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-    <Login />
-  </div> //p-4: Applies padding of 1rem (16px) on all sides of the div. h-screen: Sets the height of the div to be 100vh, making it fill the height of the screen.
+     <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<SignUp />}/>
+     </Routes>
+     <Toaster/>
+    </div> //p-4: Applies padding of 1rem (16px) on all sides of the div. h-screen: Sets the height of the div to be 100vh, making it fill the height of the screen.
   );
 }
 
