73d778ed65106215ffca2c5f3300ffd9599c3095
OlgaNas <olga.vlasova321@gmail.com>
2024-04-15 10:47:37 +0100

    Add message if no chat selected

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import Messages from './Messages'
-import MessageInput from './MessageInput'
+import React from 'react';
+import Messages from './Messages';
+import MessageInput from './MessageInput';
+import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
+  const noChatSelected = false;
   return (
     <div className='md:min-w-[450px] flex flex-col'>
+    {noChatSelected ? (<NoChatSelected />
+    ) : (
         <>
         {/* Header */}
             <div className='bg-success px-4 py-2 mb-2'>
                 <span className='label-text'>To:</span>{" "}
                 <span className='text-gray-900 font-bold'>Name Surname</span>
             </div>
-        </>
         <Messages />
-        <MessageInput />
+        <MessageInput /> 
+        </>
+    )}
+    </div>
+  )
+};
+
+
+export default MessageContainer;
+
+const NoChatSelected = () => {
+  return (
+    <div className='flex items-center justify-center w-full h-full text-white'>
+      <div className='flex flex-col items-center justify-center'>
+        <p>Welcome 👋 Name Surname 😊</p>
+        <p>Select a chat to start messaging</p>
+        <TiMessages className='text-3xl md:text-6xl text-center'/>
+      </div>
     </div>
   )
 }
-
-export default MessageContainer
 
 
 // STARTER CODE
