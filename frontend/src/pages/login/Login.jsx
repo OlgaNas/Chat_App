625a37edb6eb9931b85d54ba50fcf91682361e58
OlgaNas <olga.vlasova321@gmail.com>
2024-04-15 10:46:01 +0100

    Added text-shadow for ChatApp logo

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
         <div className='w-full p-6 rounded-lg shadow-md bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
             <h1 className='text-3xl font-semibold text-center text-gray-300'>Login 
-            <span className='text-green-500'> ChatApp</span></h1>
+            <span className='text-green-500 shadow-text'> ChatApp</span></h1>
 
             <form className='mt-5'>
                 <div>
