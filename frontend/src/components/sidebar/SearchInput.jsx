b4876def74f3a11967d33d77a8cf8b31a1a55adc
OlgaNas <olga.vlasova321@gmail.com>
2024-04-15 10:46:46 +0100

    Delete white spaces

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -5,10 +5,12 @@ import { FiSearch } from "react-icons/fi";
 const SearchInput = () => {
   return (
     <form className='flex items-center gap-2 m-3 mb-5'>
+     
         <input type="text" placeholder='Search...' className='input input-bordered rounded-full h-10 bg-gray-300' />
-        <button type='submit' className='btn btn-circle bg-success text-white hover:text-gray-900'>
+        <button type='submit' className='btn btn-circle btn-sm bg-success text-white hover:text-gray-900 '>
             <FiSearch />
         </button>
+      
     </form>
   )
 }
