0f5bcd1b25352a9060c49833099bbab11db764fc
OlgaNas <olga.vlasova321@gmail.com>
2024-04-29 12:38:21 +0100

    Show message skeleton during loading state

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import Message from './Message'
+import useGetMessages from '../../hooks/useGetMessages'
+import MessageSkeleton from '../skeletons/MessageSkeleton';
 
 const Messages = () => {
+  const {messages, loading} = useGetMessages();
+  console.log("messages: ", messages);
   return (
     <div className='px-2 flex-1 overflow-auto'>
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+       {!loading && messages.length === 0 && (
+        <p className='text-center text-white'>Send a message to start the conversation</p>
+       )}
     </div>
   )
 }
