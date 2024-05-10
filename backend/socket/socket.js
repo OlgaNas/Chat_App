c4d2110f329b660ab13d1924023459ba941cccec
OlgaNas <olga.vlasova321@gmail.com>
2024-05-10 19:16:08 +0100

    Add socket.io functionality

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,18 +7,28 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: { //CORS is a security feature 
-        origin: ["http://localhost:3000/"], // specifies which domains are allowed to access this server.
+        origin: ["http://localhost:3000"], // specifies which domains are allowed to access this server.
         methods: ["GET", "POST"] // defines which HTTP methods are allowed when accessing the resource.
     }
 }); 
-//listen for connections
+
+const userSocketMap = {};//{userId: socketId}
+//event listener for connections
 io.on('connection', (socket)=>{
     console.log("A user connected", socket.id);
 
+    const userId = socket.handshake.query.userId; //the handshake property of a socket object refers to the initial negotiation between the client and the server when a new connection is established. 
+    if(userId != "undefined") userSocketMap[userId] = socket.id;
+    
+    //emitting events to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
     //socket.on() is used to listen to the events. Can be used both on client and server side
     socket.on('disconnect', ()=>{
-        console.log("User disconnected", socket.id)
-    })
+        console.log("User disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
 })
 
 export {app, io, server};
