4c5c056dcf79471e0d3490a702259909028a1fed
OlgaNas <olga.vlasova321@gmail.com>
2024-05-19 19:20:25 +0100

    Add sound notification for a new incoming message

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {useSocketContext} from '../context/SocketContext';
 import useConversation from '../zustand/useConversation';
+import notificationSound from '../assets/sounds/sound_notification.mp3';
 
 const useListenMessages = () => {
     const {socket} = useSocketContext();
@@ -8,7 +9,10 @@ const useListenMessages = () => {
 
     useEffect(() => {
         socket?.on("newMessage", (newMessage) => {
+            //when there is a new incoming message the sound will play and the new message will shake
             newMessage.shouldShake = true;
+            const sound = new Audio(notificationSound);
+            sound.play();
             setMessages([...messages, newMessage]);
         });
         return () => socket?.off("newMessage");//clean up function
