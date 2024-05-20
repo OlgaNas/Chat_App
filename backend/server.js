94cb55b2e7f33d66fc9823658568acbf3569bbce
OlgaNas <olga.vlasova321@gmail.com>
2024-05-20 11:24:49 +0100

    Get ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";//Express Server - better performance in applications that handle a lot of concurrent connections and I/O operations, such as real-time applications
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";//The cookie-parser middleware is used to parse Cookie header and populate req.cookies with an object keyed by the cookie names
@@ -12,6 +13,8 @@ import { app, server } from './socket/socket.js';
 //const app = express(); // we relocated app to socket.js for instant messages implantation 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve(); //for deployment
+
 dotenv.config(); //to load environment variables from a .env file into process.env. 
 app.use(express.json()); // to parse the incoming requests with JSON payload (from req.body)
 app.use(cookieParser()); //This tells Express to use the cookie-parser on every request, which parses any cookies attached to the client's request
@@ -20,13 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); //for deployment
 
-
-// app.get("/", (req, res) => {
-//     //root route http://localhost:5000/
-//     res.send("Hello world!!!");
-// });
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));//This is typically used for single-page applications (SPAs) to serve the main HTML file for all routes, allowing the client-side routing to handle the rest.
+})
 
 
 
