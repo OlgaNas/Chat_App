7bca1e9e4d4129a5e17c37116f3ce2f0fcf8c2a0
OlgaNas <olga.vlasova321@gmail.com>
2024-05-21 11:13:24 +0100

    Add higher z index for logout button

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
     <div className={`fixed inset-y-0 left-0 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:translate-x-0 transition-transform duration-300 ease-in-out bg-transparent w-64 md:w-64 border-r border-slate-500 p-4 flex flex-col`}>
         <SearchInput/>
         <div className='flex-1 overflow-y-auto'><Conversations /></div>
-        <LogoutButton className='mt-auto'/>
+        <LogoutButton className='mt-auto z-51'/>
     </div>
     
     </>
