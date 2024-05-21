9c506e31d115763a786303946e008da94a4af667
OlgaNas <olga.vlasova321@gmail.com>
2024-05-21 12:46:51 +0100

    Add comment

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,8 +1,9 @@
 import { create } from 'zustand'
 //Zustand is a state management solution for React that uses hooks and allows you to create global state with a very simple and intuitive API.
 
+// Create a Zustand store for managing conversations and messages.
 const useConversation = create((set) => ({
-    selectedConversation: null,
+    selectedConversation: null, // State for the currently selected conversation
     setSelectedConversation: (selectedConversation) => set({selectedConversation}),
     messages: [],
     setMessages: (messages) => set({messages})
